f5352c3adfe2b41673203d7d3a3e144bc5725d4a
madm234 <88799492+madm234@users.noreply.github.com>
2022-06-11 13:42:22 +0530

    Updates

---
+++ precache-manifest.aaf9e0945fce7062292c1a0baf250603.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1613f99c17303f0a73a6",
+    "url": "/reactlive/static/css/main.fac6c9ef.chunk.css"
+  },
+  {
+    "revision": "1613f99c17303f0a73a6",
+    "url": "/reactlive/static/js/main.1613f99c.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/reactlive/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "c7c09eb6b7b08978aa9b",
+    "url": "/reactlive/static/js/2.c7c09eb6.chunk.js"
+  },
+  {
+    "revision": "62f27fda8e09586bb7ab",
+    "url": "/reactlive/static/js/runtime~main.62f27fda.js"
+  },
+  {
+    "revision": "05c374437876ae597c23f0bfa09f1e5c",
+    "url": "/reactlive/index.html"
+  }
+];
